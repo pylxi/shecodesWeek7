49ba63fb4b798b161b68abd0fbc2355b7ede887e
pylxi <laramontu@gmail.com>
2024-05-05 18:17:01 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,52 @@
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = city;
+
+  let currentDateElement = document.querySelector("#current-date");
+  let currentDate = new Date();
+  currentDateElement.innerHTML = formatDate(currentDate);
+
+  let apiKey = "063f17494d52a3dfa35bf52cbdeeo6at";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = `${temperature}°C`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
